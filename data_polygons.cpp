2f399302c4b1b2a004dc7f24e8db459d8488cafc
roybossofpesto <andreduracell@gmail.com>
2020-03-05 22:33:05 +0100

    black is the new green

--- data_polygons.cpp
+++ data_polygons.cpp
@@ -12,13 +12,13 @@ float colorDistance(const polygons::Color& aa, const polygons::Color& bb)
     const auto xx = std::fabs(aa.x - bb.x);
     const auto yy = std::fabs(aa.y - bb.y);
     const auto zz = std::fabs(aa.z - bb.z);
-    const auto ww = std::fabs(aa.w - bb.w);
+    const auto ww = 0.f; //std::fabs(aa.w - bb.w);
     return std::max(std::max(xx, yy), std::max(zz, ww));
 }
 
 bool polygons::isForeground(const Color& aa)
 {
-    const polygons::Color foreground_color { 0, 1, 0, 1 };
+    const polygons::Color foreground_color { 0, 0, 0, 1 };
     return colorDistance(aa, foreground_color) == 0;
 }
 
