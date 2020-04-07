e4900e8f7dfea560dd7a93f62892ffc98b75ab7e
Pierre Gueth <pierre.gueth@gmail.com>
2020-04-07 15:25:35 +0200

    tidy up

--- test_raster_window.cpp
+++ test_raster_window.cpp
@@ -117,18 +117,13 @@ class TestWindowOpenGL : public RasterWindowOpenGL
                 using std::get;
 
                 const auto aspect_ratio = width() / static_cast<float>(height());
+                const auto foo = camera_screen_height / 2;
 
                 QMatrix4x4 perspective_matrix;
-                {
-                    perspective_matrix.perspective(camera_fov_angle, aspect_ratio, get<0>(camera_clip), get<1>(camera_clip));
-                }
+                perspective_matrix.perspective(camera_fov_angle, aspect_ratio, get<0>(camera_clip), get<1>(camera_clip));
 
                 QMatrix4x4 ortho_matrix;
-                {
-                    ortho_matrix.ortho(-aspect_ratio, aspect_ratio, -1, 1, get<0>(camera_clip), get<1>(camera_clip));
-                    const auto foo = 2 / camera_screen_height;
-                    ortho_matrix.scale(foo, foo, foo);
-                }
+                ortho_matrix.ortho(-foo * aspect_ratio, foo * aspect_ratio, -foo, foo, get<0>(camera_clip), get<1>(camera_clip));
 
                 auto mixed_matrix = camera_ortho_ratio * ortho_matrix + (1 - camera_ortho_ratio) * perspective_matrix;
                 const auto camera_zz = camera_screen_height / tan(M_PI * camera_fov_angle / 180 / 2) / 2;
